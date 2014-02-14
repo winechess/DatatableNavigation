2b184d610de3309be03069ab769c8c4c4aabf18f
Sergey Vinichenko <vinichenko@imp-m.ru>
2014-02-14 16:17:21 +0400

    If last row is selected, first row will be selected next

--- src/main/webapp/resources/js/keyboard_navigation.js
+++ src/main/webapp/resources/js/keyboard_navigation.js
@@ -1,8 +1,14 @@
 var down = function() {
     //get index of selected row, if no row is selected return 0
     var index = PF('table').originRowIndex;
+    //get amount of rows in the table
+    var rows = PF('table').tbody[0].childElementCount;
     //increase row index
     index++;
+    //if new index equals number of rows, set index to first row
+    if (index === rows) {
+        index = 0;
+    }
     //deselect all selected rows
     PF('table').unselectAllRows();
     //select new row 
