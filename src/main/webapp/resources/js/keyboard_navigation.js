d1dd667eb56c7dd6df03c1f0170f11bb37f98ef6
Sergey Vinichenko <vinichenko@imp-m.ru>
2014-02-14 16:13:07 +0400

    Add down function

--- src/main/webapp/resources/js/keyboard_navigation.js
+++ src/main/webapp/resources/js/keyboard_navigation.js
@@ -1,8 +1,19 @@
+var down = function() {
+    //get index of selected row, if no row is selected return 0
+    var index = PF('table').originRowIndex;
+    //increase row index
+    index++;
+    //deselect all selected rows
+    PF('table').unselectAllRows();
+    //select new row 
+    PF('table').selectRow(index);
+};
+
 $("#table_div").keydown(function(event) {
 
     if (event.which === 40) {
         event.preventDefault();
-        alert("You press down key.");
+        down();
     }
 
 });
